58e1206c051d2da25149aaef82724f14829ee332
Vlone111 <evgenijslavkin3@gmail.com>
2025-10-15 02:47:42 +0500

    yeah

--- src/main/java/com/example/plumpet/PlumPetApplication.java
+++ src/main/java/com/example/plumpet/PlumPetApplication.java
@@ -1,4 +1,4 @@
-package com.example.plumpet.plumpet;
+package com.example.plumpet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
